e4ff51ccae564a922469be2c59e5e4700cc0a024
Teddy Kahwaji <teddykahwaji86@gmail.com>
2021-09-15 23:53:05 -0400

    created initial bot

---
+++ MusicBot.py
@@ -0,0 +1,73 @@
+import re
+from youtubesearchpython import VideosSearch
+import discord
+import asyncio
+from discord.ext import commands
+from pytube import YouTube
+import youtube_dl
+
+
+def setup(client):
+    client.add_cog(MusicBot(client))
+
+
+class MusicBot(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.queue = asyncio.Queue()
+        self.next = asyncio.Event()
+
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.send(f"You must be in voice channel! {ctx.author.mention}")
+
+        voice_channel = ctx.author.voice.channel
+
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+
+        await ctx.send(f"👍 **Joined** `{voice_channel.name}`")
+
+    @commands.command()
+    async def disconnect(self, ctx):
+        ctx.voice_client.disconnect()
+
+    @commands.command(aliases=["stop"])
+    async def pause(self, ctx):
+        ctx.voice_client.pause()
+        await ctx.send("**Paused** ⏸️")
+
+    @commands.command()
+    async def resume(self, ctx):
+        ctx.voice_client.resume()
+        await ctx.send("**Resumed**!")
+
+    @commands.command()
+    async def play(self, ctx, url):
+        await self.join(ctx)
+        YDL_OPTIONS = {'format': "bestaudio"}
+        vc = ctx.voice_client
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            song_link_regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$"
+
+            await ctx.send(f"🎵 **Searching** 🔎 `{url}`")
+            if(not re.match(song_link_regex, url)):
+                # Perform search to find video
+                videoSearch = VideosSearch(url, limit=1)
+                result = videoSearch.result()["result"]
+                if(len(result) == 0):
+                    print("No videos found when searching")
+                    await ctx.send(f"❌ could not find {url}")
+                    return
+                else:
+                    url = result[0]["link"]
+
+            info = ydl.extract_info(url, download=False)
+            url2 = info["formats"][0]["url"]
+            audio_source = discord.FFmpegPCMAudio(url2)
+            await ctx.send(f"**Playing** 🎶 `{url} -Now!`")
+            vc.play(audio_source)
+            print(info["title"])
---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+import ctypes
+import ctypes.util
+from discord.ext import commands
+from discord.errors import ClientException
+from discord_components import Button, ButtonStyle
+import MusicBot
+
+
+opus = ctypes.util.find_library('opus')
+discord.opus.load_opus(opus)
+discord.opus.is_loaded()
+
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+
+
+cogs = [MusicBot]
+intents = discord.Intents.all()
+intents.members = True
+client = commands.Bot(command_prefix="-", intents=discord.Intents.all())
+
+for cog in cogs:
+    cog.setup(client)
+
+
+@client.event
+async def on_ready():
+    print("Bot ready.")
+
+if __name__ == "__main__":
+    client.run(DISCORD_TOKEN)
