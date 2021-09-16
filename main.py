0c8951b17d084f184cb22dd22e50ca1cda1a0311
Teddy Kahwaji <teddykahwaji86@gmail.com>
2021-09-16 00:14:14 -0400

    now leaves empty channel

--- main.py
+++ main.py
@@ -29,5 +29,23 @@ for cog in cogs:
 async def on_ready():
     print("Bot ready.")
 
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    isEmptyServer = before.channel is not None and len(
+        before.channel.members) == 1
+
+    if(isEmptyServer):
+        if(member.guild.voice_client is not None):
+            print("Disconnecting from empty voice channel")
+            text_channels = member.guild.text_channels
+            text_channel_id = text_channels[0].id
+            if text_channel_id is not None:
+                channel = client.get_channel(text_channel_id)
+                await channel.send(f"**Disconnecting!** ...")
+
+            await member.guild.voice_client.disconnect()
+
+
 if __name__ == "__main__":
     client.run(DISCORD_TOKEN)
