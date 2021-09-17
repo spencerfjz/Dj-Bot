f46120e6de01e1b5aa9773b5349d230bb13e62a8
Teddy Kahwaji <teddykahwaji86@gmail.com>
2021-09-17 16:17:08 -0400

    listening

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def purge(ctx):
 @client.event
 async def on_ready():
     print("Rhythm bot here for da beats.")
-    await client.change_presence(activity=discord.Game("Da Tunes 🎵"))
+    await client.change_presence(activity=discord.ActivityType.listening, name="Da Tunes 🎵")
 
 
 @client.event
